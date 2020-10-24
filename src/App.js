820d0e18bbe5c9e51aa71250b41827a1c9bae82b
Andrew Stewart <1andrewstewart@gmail.com>
2020-10-24 08:55:43 -0400

    setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from React</h1>
+        <h2>Heya</h2>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello from React</h1>
+//     </div>
+//   );
+// }
+
+export default App;
