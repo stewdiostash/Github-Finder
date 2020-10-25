4005b4081dfb6f0a1cf98963ec6bd68b1227fa36
Andrew Stewart <1andrewstewart@gmail.com>
2020-10-24 21:46:53 -0400

    user item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar />
+        <UserItem />
       </div>
     );
   }
