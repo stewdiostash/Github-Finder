6a206b6155439db92e74e8e0ec788f291f351a6c
Andrew Stewart <1andrewstewart@gmail.com>
2020-10-24 21:01:40 -0400

    completed nav component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    const name = "John Doe";
-    const loading = true;
-
-    if (loading) {
-      return <h4>Loading...</h4>;
-    }
-
     return (
       <div className="App">
-        <h1>My App</h1>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {name}</h1>}
+        <Navbar />
       </div>
     );
   }
